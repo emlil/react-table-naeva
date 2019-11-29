eb4305b75845521eb1640a683b8d3a9623cc5962
emlil <46994670+emlil@users.noreply.github.com>
2019-11-29 13:29:38 +0100

    display images if image is present

--- src/components/myTable.js
+++ src/components/myTable.js
@@ -1,6 +1,21 @@
 import  React, {Component} from "react"
 import ReactDOM from 'react-dom';
 
+const sortTypes = {
+	up: {
+		class: 'sort-up',
+		fn: (a, b) => a.net_worth - b.net_worth
+	},
+	down: {
+		class: 'sort-down',
+		fn: (a, b) => b.net_worth - a.net_worth
+	},
+	default: {
+		class: 'sort',
+		fn: (a, b) => a
+	}
+};
+
 const apiUrl= "http://www.omdbapi.com/?apikey=4486ae48&s=";
 
 class MyTable extends  Component{
@@ -10,14 +25,8 @@ class MyTable extends  Component{
 				search:props.search,
 				list:[],
 				vals:[],
-				compiledTable:` <table style="width:100%">
-								  <tr>
-									<th>Firstname</th>
-									<th>Lastname</th>
-									<th>Age</th>
-								  </tr>
- 
-								</table> `
+				currentsort:'default',
+				compiledTable:[]
 			}
 	}
 	componentDidMount() {
@@ -30,7 +39,6 @@ class MyTable extends  Component{
 				})
 				console.log(theList);
 			})
-
 	}
 
 	objToArray(arr) {
@@ -45,18 +53,21 @@ class MyTable extends  Component{
 	}
 
 	renderCol(col) {
+		if(col.includes(".jpg"))
+			return <td><img src={col} alt={col} /></td>;
 		return <td>{col}</td>
 	}
 
 	renderItem(item) {
+
 		return <tr>{item.map(this.renderCol)}</tr>
 	}
 	renderHead(item) {
+
 		return <th>{item}</th>
 	}
 
 	render() {
-		console.log(this.state.compiledTable);
 		return(
 			<table>
 				<thead id="theHead">
